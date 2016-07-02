fc9d6900540256e69d3f862474a823abbdd8edc3
DukeJCDC <dukejcdc@gmail.com>
2016-07-01 22:39:38 -0500

    added AI player

--- Desktop/git/tictactoe.js
+++ Desktop/git/tictactoe.js
@@ -12,23 +12,46 @@ var OPlayer
 var firstPlay = 0
 var prompt = require('readline-sync');
 var board = [1,2,3,4,5,6,7,8,9];
+var pcount=2
 
 function getnames() {
+    var pcount = prompt.question("How many people are playing?");
+    pcount = parseInt(pcount);
     var XPlayer = prompt.question("X Player, enter your name: ");
-    var OPlayer = prompt.question("O Player, enter your name: ");
-    return { xname: XPlayer, oname: OPlayer };
+    if (pcount === 2) {
+        var OPlayer = prompt.question("O Player, enter your name: ");
+    } else {
+        OPlayer = "Computer";
+    };
+    return { xname: XPlayer, oname: OPlayer, players: pcount };
 };
 
 function moveCheck(move, array) {
   return array.indexOf(move) > -1;
 };
 
-function turn(player) {
-    var block = prompt.question(player + " player, make your move: ")
-    block = parseInt(block);
-     while (moveCheck(block,board) === false) {
-        block = prompt.question("Please try again: ");
+function turn(player,count) {
+    if (count === 1 && player === "O") {
+        var block = Math.floor(Math.random()*9+1);
+         while (moveCheck(block,board) === false) {
+            block = Math.floor(Math.random()*9+1);
+        };
+        
+    } else {
+        var block = prompt.question(player + " player, make your move: ");
         block = parseInt(block);
+         while (moveCheck(block,board) === false) {
+            block = prompt.question("Please try again: ");
+            block = parseInt(block);
+        };  
+    };
+    return block;
+};
+
+function AIturn() {
+    var block = Math.floor(Math.random()*9+1)
+     while (moveCheck(block,board) === false) {
+        block = Math.floor(Math.random()*9+1);
     };  
     return block;
 };
@@ -82,8 +105,8 @@ function winCheck (array) {
     };
 };
 
-function XPlay(XPlayer) {
-    XTurn = turn("X");
+function XPlay(XPlayer,pcount) {
+    XTurn = turn("X",pcount);
     XBlocks.push(XTurn);
     winCheck(XBlocks);
     fillBoard(XTurn,"X");
@@ -95,8 +118,8 @@ function XPlay(XPlayer) {
         };
 };
 
-function OPlay(OPlayer) {
-      OTurn = turn("O");
+function OPlay(OPlayer,pcount) {
+      OTurn = turn("O",pcount);
     OBlocks.push(OTurn);
     winCheck(OBlocks);
     fillBoard(OTurn,"O");
@@ -108,7 +131,7 @@ function OPlay(OPlayer) {
     }; 
 };
 
-function playGame(XPlayer, OPlayer) {
+function playGame(XPlayer, OPlayer, pcount) {
     while (playing) {
         Winner=false;
         turnNum=0;
@@ -116,12 +139,12 @@ function playGame(XPlayer, OPlayer) {
         XBlocks=[];
         OBlocks=[];
         if (Math.random() >=.5) {
-            OPlay(OPlayer);
+            OPlay(OPlayer,pcount);
         };
         while (Winner===false && turnNum<9) {
-            XPlay(XPlayer);
+            XPlay(XPlayer,pcount);
             if(Winner===false) {
-            OPlay(OPlayer);
+            OPlay(OPlayer,pcount);
                 }; 
             }; 
         if (Winner===false) {
@@ -136,4 +159,4 @@ function playGame(XPlayer, OPlayer) {
     process.exit()
 };
     var playerNames = getnames();
-    playGame(playerNames.xname, playerNames.oname); 
+    playGame(playerNames.xname, playerNames.oname,playerNames.players); 
